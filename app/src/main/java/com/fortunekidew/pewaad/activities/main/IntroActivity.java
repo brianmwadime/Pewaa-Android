05bef348e388240f3e56d1955f2b6c9f4747376c
Brian Mwakima <mwadime@fortunekidew.co.ke>
2017-10-17 22:07:42 +0300

    [B] Move contact permissions to second screen.

--- app/src/main/java/com/fortunekidew/pewaad/activities/main/IntroActivity.java
+++ app/src/main/java/com/fortunekidew/pewaad/activities/main/IntroActivity.java
@@ -31,11 +31,11 @@ public class IntroActivity extends AppIntro {
         initializerView();
 
         // Ask for CAMERA permission on the second slide
-        askForPermissions(new String[]{Manifest.permission.CAMERA}, 3); // OR
+        askForPermissions(new String[]{Manifest.permission.CAMERA}, 1); // OR
 
         // This will ask for the camera permission AND the contacts permission on the same slide.
         // Ensure your slide talks about both so as not to confuse the user.
-        askForPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 5);
+        askForPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 2);
         // Add your slide fragments here.
         // AppIntro will automatically generate the dots indicator and buttons.
         // addSlide(firstFragment);
